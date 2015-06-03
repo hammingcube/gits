19872b908c89832333020e39410d60fcf1b98e4d
maddyonline <madhavjha@gmail.com>
2015-06-03 02:58:27 -0700

    minor

--- gits.go
+++ gits.go
@@ -1,23 +1,23 @@
 package gits
 
 import (
-	"path"
-	"os"
 	"fmt"
 	"gopkg.in/pipe.v2"
+	_ "os"
+	"path"
 )
 
 type User struct {
-	Id		string
-	Name	string
+	Id   string
+	Name string
 }
 
 type GitService struct {
-	Path	string
+	Path string
 }
 
 type Config struct {
-	ServerPath	string
+	ServerPath string
 }
 
 const SERVER_PATH = "/git/data/"
@@ -32,17 +32,27 @@ func NewService(cfg *Config) *GitService {
 	return gs
 }
 
-func createRepo(path string) pipe.Pipe {
-	cmd := []string{"git", "init", "--bare"}
-	p := pipe.Line(pipe.ChDir(path), pipe.Exec(cmd[0], cmd[1:]...))
+func createRepoScript(path string) pipe.Pipe {
+	p := pipe.Script(
+		pipe.MkDirAll(path, 0777),
+		pipe.ChDir(path),
+		pipe.Exec("git", "init", "--bare"),
+	)
 	return p
 }
 
+
 func (gs *GitService) CreateRepo(repo string, user *User) (string, error) {
 	fullPath := path.Join(gs.Path, user.Name, repo+".git")
-	fmt.Println(fullPath)
-	err := os.MkdirAll(fullPath, 0777)
-	p := createRepo(fullPath)
+	p := createRepoScript(fullPath)
+	output, err := pipe.CombinedOutput(p)
+	fmt.Println(string(output))
+	return "file://" + fullPath, err
+}
+
+func (gs *GitService) AddRepo(repo string, user *User) (string, error) {
+	fullPath := path.Join(gs.Path, user.Name, repo+".git")
+	p := createRepoScript(fullPath)
 	output, err := pipe.CombinedOutput(p)
 	fmt.Println(string(output))
 	return "file://" + fullPath, err
